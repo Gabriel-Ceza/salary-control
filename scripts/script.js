87cff10b30ac2da9bc088b832daad7802766cd46
Gabriel Cristiano Ceza <gabrielzinho__o@hotmail.com>
2023-07-25 21:45:53 -0300

    criando logica do IR

--- scripts/script.js
+++ scripts/script.js
@@ -41,13 +41,32 @@ const getInssPerCent = (netSalary) => {
     }
 }
 
+const getIrPerCent = (netSalary) => {
+  if (netSalary >= 1903.99 && netSalary <= 2826.65) {
+      const irPerCent = 0.075;
+      return irPerCent;
+  } else if (netSalary >= 2826.66 && netSalary <= 3751.05) {
+      const irPerCent = 0.15 - (142.89/netSalary);
+      return irPerCent
+  } else if (netSalary >= 3751.06 && netSalary <= 4664.68) {
+      const irPerCent = 0.225 - (636.13/netSalary);
+      return irPerCent
+  } else if (netSalary >= 4664.68) {
+      const irPerCent = 0.275 - (869.36/netSalary);
+      return irPerCent
+  }
+}
+
 calculateBtn.addEventListener('click', () => {
   const netSalary = grossSalary.value - getInsalubrityValue();
   const insalubrityPercent = insalubrityPerCent(netSalary);
   const inssPercent = getInssPerCent(netSalary);
+  const irPercent = getIrPerCent(netSalary);
 
   console.log("Net Salary:", netSalary);
   console.log("Insalubrity Percentage:", insalubrityPercent);
   console.log("INSS Percentage:", inssPercent);
+  console.log("IR Percentage:", irPercent);
+
 })
 
